b7ce13578db59d55f0f6bfb5cb139b139ef32649
dattarepository <96236320+centralpavangit@users.noreply.github.com>
2023-04-19 18:18:44 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-
+if [ "$3" == "1" ]; then
+  # Run setup script only on initial clone
+  ./setup.sh
+fi
+mv .git/hooks/commit-msg.sample commit-msg
+echo "
 commit_msg_file=$1
 commit_msg=`cat $commit_msg_file`
 
 if ! echo "$commit_msg" | grep -qE '^(feat|fix|docs|style|refactor|perf|test|chore)(\(.+\))?: .{1,100}$'; then
   echo "Commit message does not follow the required format. Aborting commit."
   exit 1
-fi
+fi" >> .git/hooks/commit-msg
